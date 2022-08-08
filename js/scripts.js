97c66487b9bde7cfead9d2a28baaf53fd62c1754
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-08 15:31:08 -0700

    add inline text for window alerts for addition, subtraction, multiplication, and division in js

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,6 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals " + add(number1, number2) + ".");  
-window.alert("The subtraction of your numbers equals " + subtract(number1, number2) + ".");
-window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".");
-window.alert("The division of your numbers equals " + divide(number1, number2) + ".");
+// window.alert("The addition of your numbers equals " + add(number1, number2) + ". " + "The subtraction of your numbers equals " + subtract(number1, number2) + ". " + "The multiplication of your numbers equals " + multiply(number1, number2) + ". " + "The division of your numbers equals " + divide(number1, number2) + ".");  
+
+window.alert(number1 + "+" + number2 + " = " + add(number1, number2) + ", " + number1 + "-" + number2 + " = " + subtract(number1, number2) + ", " + number1 + "*" + number2 + " = " + multiply(number1, number2) + ", and " + number1 + "/" + number2 + " = " + divide(number1, number2));
