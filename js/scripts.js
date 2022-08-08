f495a92e089c9f922dd885bddf8eacefe413dd6a
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-08 15:10:21 -0700

    add js window alerts for addition, subtraction, division, and multiplication

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,23 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));   
+window.alert("The addition of your numbers equals " + add(number1, number2) + ".");  
+window.alert("The subtraction of your numbers equals " + subtract(number1, number2) + ".");
+window.alert("The multiplication of your numbers equals " + multiply(number1, number2) + ".");
+window.alert("The division of your numbers equals " + divide(number1, number2) + ".");
